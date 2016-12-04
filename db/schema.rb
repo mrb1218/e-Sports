493abeab7509c0f090937d9cf48eac44c3c7ed63
bhanu rayini <bhanurayini.bhanurayini@gmail.com>
2016-12-04 01:55:38 -0700

    devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161201214550) do
+ActiveRecord::Schema.define(version: 20161204083133) do
 
   create_table "favorites", force: :cascade do |t|
     t.string   "teamID"
@@ -120,9 +120,20 @@ ActiveRecord::Schema.define(version: 20161201214550) do
     t.string   "coachID"
     t.string   "region"
     t.integer  "player_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["player_id"], name: "index_users_on_player_id"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "users_roles", id: false, force: :cascade do |t|
