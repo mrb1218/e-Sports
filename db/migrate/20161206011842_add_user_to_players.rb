65bb7f24255caeccb292bdf3211edf2e84b94c1b
njarvis77 <njarvis@unm.edu>
2016-12-05 18:53:07 -0700

    change user and player table migrations fix

--- db/migrate/20161206011842_add_user_to_players.rb
+++ db/migrate/20161206011842_add_user_to_players.rb
@@ -1,5 +1,5 @@
 class AddUserToPlayers < ActiveRecord::Migration[5.0]
   def change
-    add_reference :Players, :user, foreign_key: true
+    add_reference :Players, :Users, foreign_key: true
   end
 end
