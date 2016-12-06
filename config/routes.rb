03b33ce423e781162d83fca41cd1ae87ee88a6a7
Matthew Greci <mgreci@unm.edu>
2016-12-05 21:13:21 -0700

    Home page is working and is still at pages/index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users 
-  
+  devise_for :users
   resources :matches
   resources :leagues
   resources :statistics
@@ -8,9 +7,9 @@ Rails.application.routes.draw do
   resources :players
   resources :seasons
   resources :favorites
- root 'pages#index'
 
-  get 'users/new'
+  #Home page is at pages/index.html(.erb)
+  root 'pages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
