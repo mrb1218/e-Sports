28f6ef07b786089fb19ad76f6c0aec04b07494fa
DileepKumarCheelapally <ch.dillu@gmail.com>
2016-12-12 17:22:19 -0700

    Not authorizing users other than coach and admin to register. UI fixes

--- app/policies/team_policy.rb
+++ app/policies/team_policy.rb
@@ -3,4 +3,8 @@ class TeamPolicy < ApplicationPolicy
   def create?
     user.has_role? :coach or user.has_role? :admin
  end
+
+ def new?
+   user.has_role? :coach or user.has_role? :admin
+ end
 end
